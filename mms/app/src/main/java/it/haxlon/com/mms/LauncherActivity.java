6777eb55ab24d96cc07eb8738eae52b6938ab5d2
iritainfo <irita.infotech1@gmail.com>
2018-12-05 23:03:27 +0600

    File uploaded

---
+++ mms/app/src/main/java/it/haxlon/com/mms/LauncherActivity.java
@@ -0,0 +1,13 @@
+package it.haxlon.com.mms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
